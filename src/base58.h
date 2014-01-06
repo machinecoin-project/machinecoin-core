379051ef02b7c1f280fd20754b12ed03135a6d47
Jürgen Scholz <juergen@scholz-email.de>
2014-01-06 13:34:21 +0100

    Some minor changes like changing block.nTime

--- src/base58.h
+++ src/base58.h
@@ -272,7 +272,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 48, // Machinecoin addresses start with L
+        PUBKEY_ADDRESS = 49, // Machinecoin addresses start with L
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
