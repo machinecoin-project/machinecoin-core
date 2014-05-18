ea3fa0fd951e595f27bd92f45f229af36e1f3db2
Jürgen Scholz <juergen@scholz-email.de>
2014-05-18 03:09:56 +0200

    Update info that units are mac/kb.

--- src/rpcblockchain.cpp
+++ src/rpcblockchain.cpp
@@ -106,8 +106,8 @@ Value settxfee(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() < 1 || params.size() > 1)
         throw runtime_error(
-            "settxfee <amount>\n"
-            "<amount> is a real and is rounded to the nearest 0.00000001");
+            "settxfee <amount mac/kb>\n"
+            "<amount> is a real and is rounded to the nearest 0.00000001 mac per kb");
 
     // Amount
     int64 nAmount = 0;
