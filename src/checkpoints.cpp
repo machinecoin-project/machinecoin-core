01f44d2509157e239c2773e3855a8a6f81909d88
Jürgen Scholz <juergen@scholz-email.de>
2014-11-22 22:38:07 +0100

    Added checkpoint for block 130938

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -39,11 +39,12 @@ namespace Checkpoints
         ( 23021, uint256("0x0268f4e816aac0874c911c83e263353289854c94a21cf97675652419893e7d8f"))
         ( 53600, uint256("0x327ec569aa2439c16542ed9402884f5ce691d08f49168d672f19f817ace7a06b"))
         (112715, uint256("0x4fba0d1f891a35a7e0b7370d13b777e75fd2826423ef777ccca2f63d6acc70c5"))
+        (130938, uint256("0x3fc5ccce46b45775ea3cb9f0d10169227bbd019518ebb90e5d6b8a770bf85d1d"))
         ;
     static const CCheckpointData data = {
         &mapCheckpoints,
-        1411737582, // * UNIX timestamp of last checkpoint block
-        130045,     // * total number of transactions between genesis and last checkpoint
+        1416691391, // * UNIX timestamp of last checkpoint block
+        152312,     // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
         500.0       // * estimated number of transactions per day after checkpoint
     };
