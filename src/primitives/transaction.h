40f4d24d9c76d147c528f72638da20189e503f1e
Nigho <nico.lucciola@googlemail.com>
2018-02-28 17:05:54 +0100

    Added ToStringShort() to COutPoint

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -53,6 +53,7 @@ public:
     }
 
     std::string ToString() const;
+    std::string ToStringShort() const;
 };
 
 /** An input of a transaction.  It contains the location of the previous
@@ -121,6 +122,11 @@ public:
     friend bool operator!=(const CTxIn& a, const CTxIn& b)
     {
         return !(a == b);
+    }
+    
+    friend bool operator<(const CTxIn& a, const CTxIn& b)
+    {
+        return a.prevout<b.prevout;
     }
 
     std::string ToString() const;
