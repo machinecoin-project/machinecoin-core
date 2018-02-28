96b9c0a94a9edc59079ba5dbc0d63e7829e79f88
Nigho <nico.lucciola@googlemail.com>
2018-02-28 17:35:53 +0100

    Removed DIP0001 reference & don't override not existing functions

--- src/dsnotificationinterface.h
+++ src/dsnotificationinterface.h
@@ -18,10 +18,10 @@ public:
 
 protected:
     // CValidationInterface
-    void AcceptedBlockHeader(const CBlockIndex *pindexNew) override;
-    void NotifyHeaderTip(const CBlockIndex *pindexNew, bool fInitialDownload) override;
+    void AcceptedBlockHeader(const CBlockIndex *pindexNew);
+    void NotifyHeaderTip(const CBlockIndex *pindexNew, bool fInitialDownload);
     void UpdatedBlockTip(const CBlockIndex *pindexNew, const CBlockIndex *pindexFork, bool fInitialDownload) override;
-    void SyncTransaction(const CTransaction &tx, const CBlock *pblock) override;
+    void SyncTransaction(const CTransaction &tx, const CBlock *pblock);
 
 private:
     CConnman& connman;
