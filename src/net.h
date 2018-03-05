65818db28117606cdb9a6be2c651a3e4a30b5434
Nigho <nico.lucciola@googlemail.com>
2018-03-05 19:10:21 +0100

    Reworked in core 0.13 so remove

--- src/net.h
+++ src/net.h
@@ -594,9 +594,6 @@ extern bool fDiscover;
 extern bool fListen;
 extern bool fRelayTxes;
 
-extern std::map<CInv, CDataStream> mapRelay;
-extern std::deque<std::pair<int64_t, CInv> > vRelayExpiration;
-extern CCriticalSection cs_mapRelay;
 extern limitedmap<uint256, int64_t> mapAlreadyAskedFor;
 
 /** Subversion as sent to the P2P network in `version` messages */
