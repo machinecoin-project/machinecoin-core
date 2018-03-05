407a4916aa1734b8a4279eef5ad315b2bbe1c769
Nigho <nico.lucciola@googlemail.com>
2018-03-05 23:33:45 +0100

    TX to TXRef

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -807,7 +807,7 @@ public:
     CAmount GetUnconfirmedWatchOnlyBalance() const;
     CAmount GetImmatureWatchOnlyBalance() const;
     
-    bool GetBudgetSystemCollateralTX(CTransaction& tx, uint256 hash, CAmount amount);
+    bool GetBudgetSystemCollateralTX(CTransactionRef& tx, uint256 hash, CAmount amount);
     bool GetBudgetSystemCollateralTX(CWalletTx& tx, uint256 hash, CAmount amount);
 
     /**
