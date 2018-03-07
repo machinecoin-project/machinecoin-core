437c66a3960a7196ca644907a97b1841275f37a8
Nigho <nico.lucciola@googlemail.com>
2018-03-07 18:00:00 +0100

    Added missing members to Block object

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -78,6 +78,8 @@ public:
     std::vector<CTransactionRef> vtx;
 
     // memory only
+    mutable CTxOut txoutMasternode; // masternode payment
+    mutable std::vector<CTxOut> voutSuperblock; // superblock payment
     mutable bool fChecked;
 
     CBlock()
@@ -103,6 +105,8 @@ public:
     {
         CBlockHeader::SetNull();
         vtx.clear();
+        txoutMasternode = CTxOut();
+        voutSuperblock.clear();
         fChecked = false;
     }
 
