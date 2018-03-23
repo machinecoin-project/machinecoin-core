769d896ceabfcd09a60d8b857b5b030eed1d7a51
Nigho <nico.lucciola@googlemail.com>
2018-03-23 19:27:57 +0100

    vector no set

--- src/net.h
+++ src/net.h
@@ -771,7 +771,7 @@ public:
     // Set of transaction ids we still have to announce.
     // They are sorted by the mempool before relay, so the order is not important.
     std::set<uint256> setInventoryTxToSend;
-    std::set<CInv> vInventoryMNToSend;
+    std::vector<CInv> vInventoryMNToSend;
     // List of block ids we still have announce.
     // There is no final sorting before sending, as they are always sent immediately
     // and in the order requested.
