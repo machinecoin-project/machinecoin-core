c6d835858a09b62312b3fca5cb1a189d526fd6d0
Nigho <nico.lucciola@googlemail.com>
2018-03-27 16:55:45 +0200

    Convert to string first

--- src/net.cpp
+++ src/net.cpp
@@ -2922,8 +2922,10 @@ void CConnman::PushMessage(CNode* pnode, CSerializedNetMsg&& msg)
     size_t nTotalSize = nMessageSize + CMessageHeader::HEADER_SIZE;
     LogPrint("net", "sending %s (%d bytes) peer=%d\n",  SanitizeString(msg.command.c_str()), nMessageSize, pnode->id);
     
-    if (SanitizeString(msg.command.c_str()) == "inv")
-        LogPrintf("sending inv: %s", msg.data.type);
+    if (SanitizeString(msg.command.c_str()) == "inv") {
+        std::string str(msg.data.begin(), msg.data.end())
+        LogPrintf("sending inv: %s", str);
+    }
 
     std::vector<unsigned char> serializedHeader;
     serializedHeader.reserve(CMessageHeader::HEADER_SIZE);
