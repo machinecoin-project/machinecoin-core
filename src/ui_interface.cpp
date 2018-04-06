3aae0665b4f78191ec87cfd0219beb5ec70e62e5
Nigho <nico.lucciola@googlemail.com>
2018-04-06 10:00:22 +0200

    Copyright year changed to 2018

--- src/ui_interface.cpp
+++ src/ui_interface.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2010-2017 The Machinecoin Core developers
+﻿// Copyright (c) 2010-2018 The Machinecoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
