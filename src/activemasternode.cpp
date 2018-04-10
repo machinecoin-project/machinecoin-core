0b42a218b6c592ffbaddfe889d2f8e992b615a7d
Nigho <nico.lucciola@googlemail.com>
2018-04-10 07:45:04 +0200

    other variable name

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -166,7 +166,7 @@ void CActiveMasternode::ManageStateInitial(CConnman& connman)
         return;
     }
 
-    int defaultChainParams = CreateChainParams(CBaseChainParams::MAIN)->GetDefaultPort();
+    int mainnetDefaultPort = CreateChainParams(CBaseChainParams::MAIN)->GetDefaultPort();
     if(Params().NetworkIDString() == CBaseChainParams::MAIN) {
         if(service.GetPort() != mainnetDefaultPort) {
             nState = ACTIVE_MASTERNODE_NOT_CAPABLE;
