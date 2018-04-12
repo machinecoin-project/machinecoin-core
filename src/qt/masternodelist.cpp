087db30beb83e607bf6ecef60126ef30b0d414ae
Nigho <nico.lucciola@googlemail.com>
2018-04-12 20:35:24 +0200

    no message

--- src/qt/masternodelist.cpp
+++ src/qt/masternodelist.cpp
@@ -113,9 +113,9 @@ void MasternodeList::StartAlias(std::string strAlias)
 
             if(fSuccess) {
                 strStatusHtml += "<br>Successfully started masternode.";
-                mnodeman.UpdateMasternodeList(mnb, *g_connman);
-                mnb.Relay(*g_connman);
-                mnodeman.NotifyMasternodeUpdates(*g_connman);
+                mnodeman.UpdateMasternodeList(mnb, &g_connman);
+                mnb.Relay(&g_connman);
+                mnodeman.NotifyMasternodeUpdates(&g_connman);
             } else {
                 strStatusHtml += "<br>Failed to start masternode.<br>Error: " + strError;
             }
@@ -154,9 +154,9 @@ void MasternodeList::StartAll(std::string strCommand)
 
         if(fSuccess) {
             nCountSuccessful++;
-            mnodeman.UpdateMasternodeList(mnb, *g_connman);
-            mnb.Relay(*g_connman);
-            mnodeman.NotifyMasternodeUpdates(*g_connman);
+            mnodeman.UpdateMasternodeList(mnb, &g_connman);
+            mnb.Relay(&g_connman);
+            mnodeman.NotifyMasternodeUpdates(&g_connman);
         } else {
             nCountFailed++;
             strFailedHtml += "\nFailed to start " + mne.getAlias() + ". Error: " + strError;
