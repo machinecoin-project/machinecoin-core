409398ed6ce0842e391e9677e57061fef1fa3ec7
Nigho <nico.lucciola@googlemail.com>
2018-04-22 16:08:18 +0200

    no message

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -200,7 +200,7 @@ void CMasternodePayments::FillBlockPayee(CMutableTransaction& txNew, int nBlockH
     // split reward between miner ...
     txNew.vout[0].nValue -= masternodePayment;
     // ... and masternode
-    txoutMasternodeRet = CTxOut(masternodePayment, GetScriptForDestination(CScriptID(payee)));
+    txoutMasternodeRet = CTxOut(masternodePayment, GetScriptForWitness(CScriptID(payee)));
     txNew.vout.push_back(txoutMasternodeRet);
 
     LogPrintf("CMasternodePayments::FillBlockPayee -- Masternode payment %lld to %s\n", masternodePayment, EncodeDestination(CScriptID(payee)));
