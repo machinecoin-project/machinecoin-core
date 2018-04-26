6aa2dd9980698bf87f050ff5f982dde1fc990746
Nigho <nico.lucciola@googlemail.com>
2018-04-26 14:36:11 +0200

    no message

--- src/init.cpp
+++ src/init.cpp
@@ -1710,7 +1710,7 @@ bool AppInitMain()
             if(!CMessageSigner::GetKeysFromSecret(strMasterNodePrivKey, activeMasternode.keyMasternode, activeMasternode.pubKeyMasternode))
                 return InitError(_("Invalid masternodeprivkey. Please see documenation."));
 
-            LogPrintf("  pubKeyMasternode: %s\n", EncodeDestination(GetScriptForDestination(WitnessV0KeyHash(pubKeyCollateralAddress.GetID()))));
+            LogPrintf("  pubKeyMasternode: %s\n", EncodeDestination(GetScriptForDestination(WitnessV0KeyHash(activeMasternode.pubKeyMasternode.GetID()))));
         } else {
             return InitError(_("You must specify a masternodeprivkey in the configuration. Please see documentation for help."));
         }
