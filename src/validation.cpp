2d76ead23d124924cb167ffe3a585ebbb0a5a811
Nigho <nico.lucciola@googlemail.com>
2018-04-26 13:11:04 +0200

    no message

--- src/validation.cpp
+++ src/validation.cpp
@@ -2028,7 +2028,7 @@ bool CChainState::ConnectBlock(const CBlock& block, CValidationState& state, CBl
     LogPrint(MCLog::BENCH, "      - Connect %u transactions: %.2fms (%.3fms/tx, %.3fms/txin) [%.2fs (%.2fms/blk)]\n", (unsigned)block.vtx.size(), MILLI * (nTime3 - nTime2), MILLI * (nTime3 - nTime2) / block.vtx.size(), nInputs <= 1 ? 0 : MILLI * (nTime3 - nTime2) / (nInputs-1), nTimeConnect * MICRO, nTimeConnect * MILLI / nBlocksTotal);
 
     CAmount blockReward = nFees + GetBlockSubsidy(pindex->nHeight, chainparams.GetConsensus());
-    if (!CSuperblockManager::IsSuperblockTriggered(pindex->nHeight))
+    if (!CSuperblockManager::IsValidBlockHeight(pindex->nHeight))
         if (block.vtx[0]->GetValueOut() > blockReward)
             return state.DoS(100,
                              error("ConnectBlock(): coinbase pays too much (actual=%d vs limit=%d)",
