25a35c3d705dcfafdb648e40e2b30eba8e4e76cf
Nigho <nico.lucciola@googlemail.com>
2018-05-04 19:36:59 +0200

    Updated machinecoin.org to machinecoin.io

--- src/test/netbase_tests.cpp
+++ src/test/netbase_tests.cpp
@@ -82,10 +82,10 @@ bool static TestSplitHost(std::string test, std::string host, int port)
 
 BOOST_AUTO_TEST_CASE(netbase_splithost)
 {
-    BOOST_CHECK(TestSplitHost("www.machinecoin.org", "www.machinecoin.org", -1));
-    BOOST_CHECK(TestSplitHost("[www.machinecoin.org]", "www.machinecoin.org", -1));
-    BOOST_CHECK(TestSplitHost("www.machinecoin.org:80", "www.machinecoin.org", 80));
-    BOOST_CHECK(TestSplitHost("[www.machinecoin.org]:80", "www.machinecoin.org", 80));
+    BOOST_CHECK(TestSplitHost("www.machinecoin.io", "www.machinecoin.io", -1));
+    BOOST_CHECK(TestSplitHost("[www.machinecoin.io]", "www.machinecoin.io", -1));
+    BOOST_CHECK(TestSplitHost("www.machinecoin.io:80", "www.machinecoin.io", 80));
+    BOOST_CHECK(TestSplitHost("[www.machinecoin.io]:80", "www.machinecoin.io", 80));
     BOOST_CHECK(TestSplitHost("127.0.0.1", "127.0.0.1", -1));
     BOOST_CHECK(TestSplitHost("127.0.0.1:8333", "127.0.0.1", 8333));
     BOOST_CHECK(TestSplitHost("[127.0.0.1]", "127.0.0.1", -1));
