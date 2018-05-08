deac5631e8596eb5049b6bda3de37c1d0e5bbec7
Nigho <nico.lucciola@googlemail.com>
2018-05-08 15:08:20 +0200

    Use .cpp instead of .h here

--- src/test/test_machinecoin.cpp
+++ src/test/test_machinecoin.cpp
@@ -2,8 +2,6 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#define BOOST_TEST_DYN_LINK
-
 #include <test/test_machinecoin.h>
 
 #include <chainparams.h>
