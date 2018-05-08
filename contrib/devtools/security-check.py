b6653322b8538ae058b25a6cab5c6162300bd93d
Nigho <nico.lucciola@googlemail.com>
2018-05-08 18:53:16 +0200

    Change file encoding

--- contrib/devtools/security-check.py
+++ contrib/devtools/security-check.py
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/env python
+#!/usr/bin/env python
 # Copyright (c) 2015-2017 The Machinecoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
