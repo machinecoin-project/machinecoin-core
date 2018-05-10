2f246546c3cae46c6f434e2bd20ea34f2cd322de
Nigho <nico.lucciola@googlemail.com>
2018-05-10 18:19:00 +0200

    Changed file encoding and removed weired b

--- contrib/macdeploy/extract-osx-sdk.sh
+++ contrib/macdeploy/extract-osx-sdk.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/bash
+#!/bin/bash
 # Copyright (c) 2016 The Machinecoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
