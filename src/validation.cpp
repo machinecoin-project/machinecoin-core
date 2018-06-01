9c43f7cb23aca8d8b2b38ceb682ce0199aecbde3
Nigho <nico.lucciola@googlemail.com>
2018-06-01 10:29:08 +0200

    validation: fix logic issue here

--- src/validation.cpp
+++ src/validation.cpp
@@ -3102,7 +3102,7 @@ bool CheckBlock(const CBlock& block, CValidationState& state, const Consensus::P
     if (nSigOps * WITNESS_SCALE_FACTOR > MAX_BLOCK_SIGOPS_COST)
         return state.DoS(100, false, REJECT_INVALID, "bad-blk-sigops", false, "out-of-bounds SigOpCount");
 
-    if (consensusParams.fEnforceMasternodePayments && consensusParams.nMasternodePaymentsStartBlock > chainActive.Tip()->nHeight) {
+    if (consensusParams.fEnforceMasternodePayments && chainActive.Tip()->nHeight > consensusParams.nMasternodePaymentsStartBlock) {
         if(!mnpayments.IsTransactionValid(block.vtx[0], chainActive.Tip()->nHeight + 1, block.vtx[0]->GetValueOut())) {
             return state.DoS(100, false, REJECT_INVALID, "bad-cb-payment", false, "no masternode payment (enforced)");
         }
