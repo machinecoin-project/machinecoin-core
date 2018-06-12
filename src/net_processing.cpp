17d7adbd47f09eabc35dc77323cb3c38c1666a85
Nico Lucciola <nico.lucciola@gmail.com>
2018-06-12 11:08:38 +0200

    net: updated logging lines, to only display if log level is NET or 1

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1900,7 +1900,7 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
             // Mark this node as currently connected, so we update its timestamp later.
             LOCK(cs_main);
             State(pfrom->GetId())->fCurrentlyConnected = true;
-            LogPrintf("New outbound peer connected: version: %d, blocks=%d, peer=%d%s\n",
+            LogPrint(MCLog::NET, "New outbound peer connected: version: %d, blocks=%d, peer=%d%s\n",
                       pfrom->nVersion.load(), pfrom->nStartingHeight, pfrom->GetId(),
                       (fLogIPs ? strprintf(", peeraddr=%s", pfrom->addr.ToString()) : ""));
         }
