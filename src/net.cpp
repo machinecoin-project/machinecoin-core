70cbb0f5cd0fc4a4090a92f41d725febdc7c0552
Nico Lucciola <nico.lucciola@gmail.com>
2018-11-03 20:18:23 +0100

    added missing expressions

--- src/net.cpp
+++ src/net.cpp
@@ -77,6 +77,9 @@ enum BindFlags {
 
 const static std::string NET_MESSAGE_COMMAND_OTHER = "*other*";
 
+constexpr const CConnman::CFullyConnectedOnly CConnman::FullyConnectedOnly;
+constexpr const CConnman::CAllNodes CConnman::AllNodes;
+
 static const uint64_t RANDOMIZER_ID_NETGROUP = 0x6c0edd8036ef4036ULL; // SHA256("netgroup")[0:8]
 static const uint64_t RANDOMIZER_ID_LOCALHOSTNONCE = 0xd93e69e2bbfa5735ULL; // SHA256("localhostnonce")[0:8]
 //
